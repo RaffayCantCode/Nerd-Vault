a00232e2be8bf8b48cd7ebf2c9cad64a66dcb77f
Raffay <asifraffy@gmail.com>
2026-05-30 15:45:06 +0500

    logo fix!

--- src/app/apple-icon.tsx
+++ src/app/apple-icon.tsx
@@ -13,13 +13,15 @@ export default function AppleIcon() {
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          background: "#0b1220",
+          background: "#050914",
           borderRadius: 40,
         }}
       >
-        <svg width="130" height="130" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
-          <path d="M215 148H442L547 309V384L443 225H300V555H215V148Z" fill="#69BEAA"/>
-          <path d="M431 284L537 447H677V148H764V554H536L431 392V284Z" fill="#2CB8C1"/>
+        <svg width="180" height="180" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
+          <g transform="translate(-148 38) scale(1.35)">
+            <path d="M215 148H442L547 309V384L443 225H300V555H215V148Z" fill="#69BEAA" />
+            <path d="M431 284L537 447H677V148H764V554H536L431 392V284Z" fill="#2CB8C1" />
+          </g>
         </svg>
       </div>
     ),
